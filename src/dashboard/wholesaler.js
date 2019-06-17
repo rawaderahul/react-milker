1a91282ed3bfd3dbb18ab136b39f15d21079da28
Yogesh Jadhav <yogesh18.ivision@gmail.com>
2019-06-17 15:07:16 +0530

    update some changes'

--- src/dashboard/wholesaler.js
+++ src/dashboard/wholesaler.js
@@ -127,11 +127,11 @@ class AnyRoutes extends Component {
             width:'15%',
             render: (text, record) => (
                 <span>
-                <Icon type="minus" onClick={() => this.decrement(record.id,'buffalo')}/>
-                    <InputNumber value = {record.buffalo} 
-                    style={{width:60,marginLeft:10,marginRight:10,textAlign:'center'}}
-                    onChange={(e)=> this.handleChange(e,record.id,'buffalo')}/>
-                <Icon type="plus" onClick={() => this.increament(record.id,'buffalo')} style={{fontWeight:'bolder'}}/>
+                    <Icon type="minus" onClick={() => this.decrement(record.id,'buffalo')}/>
+                        <InputNumber value = {record.buffalo} 
+                        style={{width:60,marginLeft:10,marginRight:10,textAlign:'center'}}
+                        onChange={(e)=> this.handleChange(e,record.id,'buffalo')}/>
+                    <Icon type="plus" onClick={() => this.increament(record.id,'buffalo')} style={{fontWeight:'bolder'}}/>
                 </span>
             ),
             align:'center',
@@ -148,20 +148,19 @@ class AnyRoutes extends Component {
         {
             title:'cow',
             title: 'Cow',
-            width:'20%',
+            width:'15%',
             render: (text, record) => (
                 <span>
-                <Icon type="minus" onClick={() => this.decrement(record.id,'cow')}/>
-                <InputNumber defaultValue={record.cow} value={record.cow} 
-                    style={{width:60,marginLeft:10,marginRight:10,textAlign:'center'}} 
-                    onChange={(e)=> this.handleChange(e,record.id,'cow')}/>
-                <Icon type="plus" onClick={() => this.increament(record.id,'cow')} style={{fontWeight:'bolder'}}/>
+                    <Icon type="minus" onClick={() => this.decrement(record.id,'cow')}/>
+                    <InputNumber defaultValue={record.cow} value={record.cow} 
+                        style={{width:30,marginLeft:10,marginRight:10,textAlign:'center'}} 
+                        onChange={(e)=> this.handleChange(e,record.id,'cow')}/>
+                    <Icon type="plus" onClick={() => this.increament(record.id,'cow')} style={{fontWeight:'bolder'}}/>
                 </span>
             ),
             dataIndex:'cow',
             editable:true,
             align:'center'
-
         },
         {
             title: 'Cow Price',
@@ -217,17 +216,17 @@ class AnyRoutes extends Component {
 
     increament=(id,text)=>{
         const { dataSource }=this.state;
-    dataSource.map((item) => {
-        if(item.id==id) {
-            switch(text) {
-            case 'buffalo': item.buffalo=item.buffalo + 0.5;
-            break;
-            case 'cow': item.cow=item.cow + 0.5;
-            break;
+        dataSource.map((item) => {
+            if(item.id==id) {
+                switch(text) {
+                case 'buffalo': item.buffalo=item.buffalo + 0.5;
+                break;
+                case 'cow': item.cow=item.cow + 0.5;
+                break;
+                }
             }
-        }
-    })
-    this.setState({dataSource})
+        })
+        this.setState({dataSource})
 
         dataSource.map((datas)=>{
             this.state.milkbrand.map((milk)=>{
@@ -238,7 +237,6 @@ class AnyRoutes extends Component {
             })
         })
         this.setState({TotalBuffaloPrice : BuffaloPrice});
-
     }
   
     decrement=(id,text)=> {
